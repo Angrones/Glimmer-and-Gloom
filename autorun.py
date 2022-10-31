41483d56035c94246dff97a376341e867e21a418
HjOtal-1 <102916830+HjOtal-1@users.noreply.github.com>
2022-10-31 20:21:45 +0000

    Fixed "Beginning game +10"
    Both Beginning game 11 and str error have been fixed, and rephrased a few thing.

--- autorun.py
+++ autorun.py
@@ -9,11 +9,11 @@ GAME_DELAY_1 = 2.9 # How much we wait after the game loads.
 GAME_DELAY_2 = .54 # How much we wait before starting a new game.
 CONFIDENCE_VALUE = .9 # The confidence value we're searching the screen with. A lower number will lead to more false positives.
 NUM_LOOPS = 19 # The maximum number of games we'll play. After this number, the script will quit.
-n = 0
+n = 1
 
-print("beginning to play the game. We'll play " + str(NUM_LOOPS) + " games.")
+print("Starting the game, we'll play " + str(NUM_LOOPS) + " rounds.")
 while n < NUM_LOOPS:
-	print("beginning game " + str(n) + str(1) + "...")
+	print("Beginning round #" + str(n) + "...")
 	solve_board()
 	time.sleep(GAME_DELAY_1)
 	pyautogui.click(
@@ -25,6 +25,6 @@ while n < NUM_LOOPS:
 		)
 	)
 	time.sleep(GAME_DELAY_2)
-	print("ending game " + str(n) + str(1) + "...")
+	print("Ending round #" + str(n) + "...")
 	n += 1
-print("Script exiting.")
+print("Exiting Script.")
