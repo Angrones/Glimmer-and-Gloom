5543e5255b571d96d9230e8ec4549f5033319cf1
Angrones <102916830+Angrones@users.noreply.github.com>
2024-06-25 20:06:07 +0100

    Fixed the counter bug
    Apparently, the fix I did actually broke the counter, resulting in the script ending at 18th turn, forcing users to add 20 turns instead.
    
    The counter has since been fixed, and it now should do exactly 19 turns, and end *after* the 19th turn.

--- autorun.py
+++ autorun.py
@@ -2,17 +2,18 @@
 
 import pyautogui
 import time
-from board_solver import *
+import keyboard
+from solver import *
 
 PLAY_FILE_LOC = "play.png" # The local file location of the play button image
 GAME_DELAY_1 = 2.9 # How much we wait after the game loads.
 GAME_DELAY_2 = .54 # How much we wait before starting a new game.
 CONFIDENCE_VALUE = .9 # The confidence value we're searching the screen with. A lower number will lead to more false positives.
 NUM_LOOPS = 19 # The maximum number of games we'll play. After this number, the script will quit.
-n = 1
+n = 1 # DO NOT CHANGE THIS!!!!
 
 print("Starting the game, we'll play " + str(NUM_LOOPS) + " rounds.")
-while n < NUM_LOOPS:
+while n <= NUM_LOOPS:
 	print("Beginning round #" + str(n) + "...")
 	solve_board()
 	time.sleep(GAME_DELAY_1)
